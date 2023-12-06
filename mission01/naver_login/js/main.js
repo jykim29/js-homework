92bc834c6bad9ada0e4dcb7b02429816d00fb3e2
jykim29 <skt13181@naver.com>
2023-12-06 21:11:06 +0900

    [modif] 1. 정규식을 사용한 email, password validation 함수 작성 2. 주어진 user객체 값과 입력한 email, password 값이 같은지 비교하는 함수 작성 3. 로그인 form submit시 조건에 따라 처리하는 함수 작성

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,69 @@ const user = {
 
 */
 
-function emailReg(text){
+function emailReg(text) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+(function () {
+  "use strict";
+  const $userEmailInput = document.querySelector(".user-email-input");
+  const $userPasswordInput = document.querySelector(".user-password-input");
+  const $loginForm = document.querySelector(".login-form");
 
+  // input 요소 input 이벤트 핸들러
+  function inputHandler(e) {
+    inputValidation(this);
+  }
 
+  // form 요소 submit 이벤트 핸들러
+  function submitHandler(e) {
+    e.preventDefault();
+    login(
+      e.target,
+      () => navigate("./welcome.html"),
+      () => alert("아이디 또는 비밀번호가 올바르지 않습니다.")
+    );
+  }
 
+  // 1. email 정규표현식을 사용한 validation
+  // 2. pw 정규표현식을 사용한 validation
+  function inputValidation(node) {
+    const { id, value } = node;
+    const isValid = id === "userEmail" ? emailReg(value) : pwReg(value);
+    if (isValid) {
+      node.classList.remove("is--invalid");
+    } else {
+      node.classList.add("is--invalid");
+    }
+  }
 
+  // 3. 상태 변수 관리
+  function compare(email, password) {
+    const { id, pw } = user;
+    return email === id && password === pw ? true : false;
+  }
 
+  // 4. 로그인 버튼을 클릭시 조건처리
+  function login(form, success, fail) {
+    const { userEmail: email, userPassword: password } = form;
+    const isCorrect = compare(email.value, password.value);
+    return isCorrect ? success() : fail();
+  }
 
+  // 페이지 이동
+  function navigate(url) {
+    window.location.href = url;
+  }
 
-
-
-
+  $userEmailInput.addEventListener("input", inputHandler);
+  $userPasswordInput.addEventListener("input", inputHandler);
+  $loginForm.addEventListener("submit", submitHandler);
+})();
