bf465d1bbc55ea6e2067a38ec885ca7beeb8d790
jykim29 <skt13181@naver.com>
2023-12-08 00:25:02 +0900

    [modif] 1. valid 상태 관리 객체 추가 2. inputValidation 로직 변경 (상태관리 객체 활용) 3. invalid 상태 클래스 토글 함수 분리 4. 클로저를 사용한 bindEvent 함수 작성(미사용)

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -24,18 +24,26 @@ function pwReg(text) {
 
 (function () {
   "use strict";
+  // DOM element
   const $userEmailInput = document.querySelector(".user-email-input");
   const $userPasswordInput = document.querySelector(".user-password-input");
   const $loginForm = document.querySelector(".login-form");
+
+  // 객체를 사용한 email과 password의 valid 상태 관리
+  const isValidObj = {
+    userEmail: false,
+    userPassword: false,
+  };
+  // 페이지 첫 로드 시 email input 요소에 포커스
   $userEmailInput.focus();
 
   // input 요소 input 이벤트 핸들러
-  function inputHandler(e) {
-    inputValidation(this);
+  function handleInput(e) {
+    inputValidation(e.target);
   }
 
   // form 요소 submit 이벤트 핸들러
-  function submitHandler(e) {
+  function handleSubmit(e) {
     e.preventDefault();
     login(
       e.target,
@@ -48,7 +56,12 @@ function pwReg(text) {
   // 2. pw 정규표현식을 사용한 validation
   function inputValidation(node) {
     const { id, value } = node;
-    const isValid = id === "userEmail" ? emailReg(value) : pwReg(value);
+    isValidObj[id] = id === "userEmail" ? emailReg(value) : pwReg(value);
+    return toggleInvalidStateClass(node, isValidObj[id]);
+  }
+
+  // invalid 상태 클래스 토글
+  function toggleInvalidStateClass(node, isValid) {
     if (isValid) {
       node.classList.remove("is--invalid");
     } else {
@@ -56,16 +69,16 @@ function pwReg(text) {
     }
   }
 
-  // 3. 상태 변수 관리
-  function compare(email, password) {
-    const { id, pw } = user;
-    return email === id && password === pw ? true : false;
+  // user 객체 값과 입력한 값 비교
+  function compare(emailValue, passwordValue) {
+    const { id: userId, pw: userPw } = user;
+    return emailValue === userId && passwordValue === userPw ? true : false;
   }
 
   // 4. 로그인 버튼을 클릭시 조건처리
   function login(form, success, fail) {
-    const { userEmail: email, userPassword: password } = form;
-    const isCorrect = compare(email.value, password.value);
+    const { userEmail: inputEmail, userPassword: inputPassword } = form;
+    const isCorrect = compare(inputEmail.value, inputPassword.value);
     return isCorrect ? success() : fail();
   }
 
@@ -75,7 +88,26 @@ function pwReg(text) {
   }
 
   [$userEmailInput, $userPasswordInput].forEach((node) => {
-    node.addEventListener("input", inputHandler);
+    node.addEventListener("input", handleInput);
   });
-  $loginForm.addEventListener("submit", submitHandler);
+  $loginForm.addEventListener("submit", handleSubmit);
+
+  /* 클로저 연습 겸 만들어 본 bindEvent 함수
+  function bindEvent(nodes, type, handler) {
+    if (Array.isArray(nodes)) {
+      nodes.forEach((node) => node.addEventListener(type, handler));
+    } else {
+      nodes.addEventListener(type, handler);
+    }
+    return () => {
+      if (Array.isArray(nodes)) {
+        nodes.forEach((node) => node.removeEventListener(type, handler));
+      } else {
+        nodes.removeEventListener(type, handler);
+      }
+    };
+  }
+  const removeInputEvent = bindEvent([$userEmailInput, $userPasswordInput], "input", handleInput);
+  const removeSubmitEvent = bindEvent($loginForm, "submit", handleSubmit);
+  */
 })();
