164211cb637fc0a039ddc44094de0975768d4e09
jykim29 <skt13181@naver.com>
2023-12-14 23:36:54 +0900

    1. li 클릭시 오디오 재생

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -4,15 +4,18 @@
 2. nav 클릭시 배경 색상 변경
 3. 이미지 변경
 4. 텍스트 변경
-5. 함수 분리
+5. 오디오 재생 
+6. 함수 분리
 
 */
 
 (() => {
   // 기본 URL
-  const BASE_URL = "./assets/";
+  const BASE_URL = "./assets";
   // 이벤트 처리 방식
   const EVENT_MODE = "delegation"; // 'delegation' | 'loop'
+  // 오디오 객체
+  const audio = new Audio();
 
   // 이벤트 위임 방식
   if (EVENT_MODE === "delegation") getNode(".nav").addEventListener("click", handleClick);
@@ -28,6 +31,7 @@
     setBgColor(currentData);
     setImage(getNode(".visual img"), currentData);
     setNameText(getNode(".nickName"), currentData);
+    playSound(currentData);
   }
 
   function setClassName(elem) {
@@ -41,11 +45,19 @@
   }
 
   function setImage(elem, data) {
-    elem.src = BASE_URL + data.name.toLowerCase() + ".jpeg";
+    elem.src = `${BASE_URL}/${data.name.toLowerCase()}.jpeg`;
+
     elem.alt = data.alt;
   }
 
   function setNameText(elem, data) {
     elem.textContent = data.name;
+  }
+
+  function playSound(data) {
+    audio.src = `${BASE_URL}/audio/${data.name.toLowerCase()}.m4a`;
+    if (data.name === "WADE" || data.name === "GALE") audio.volume = 0.2; // ear protect
+    else audio.volume = 1;
+    audio.play();
   }
 })();
