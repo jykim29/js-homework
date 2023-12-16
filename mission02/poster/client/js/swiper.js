3b3c25533fc82c215f025e536692a0df6cec5521
jykim29 <skt13181@naver.com>
2023-12-16 22:21:07 +0900

    [feat] Swiper.js 를 사용한 슬라이드 애니메이션 효과 구현

---
+++ mission02/poster/client/js/swiper.js
@@ -0,0 +1,57 @@
+(() => {
+  // 기본 URL
+  const BASE_URL = "./assets";
+  // 오디오 객체
+  const audio = new Audio();
+  audio.muted = true;
+
+  const swiper = new Swiper(".swiper", {
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: ".pagination",
+      type: "bullets",
+      clickable: true,
+      bulletClass: "bullet",
+      bulletActiveClass: "is-active",
+      renderBullet(index, className) {
+        return /*html*/ `<span class="${className}"><img src="${BASE_URL}/${data[index].name.toLowerCase()}.jpeg" /></span>`;
+      },
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    effect: "cube",
+    loop: true,
+  });
+
+  swiper.on("realIndexChange", (swiper) => {
+    const index = swiper.realIndex;
+    setBgColor(data[index]);
+    playSound(data[index]);
+  });
+
+  function setBgColor(data) {
+    document.body.style.background = `linear-gradient(to bottom, ${data.color[0]}, ${data.color[1] || "#000"})`;
+  }
+  function playSound(data) {
+    audio.src = `${BASE_URL}/audio/${data.name.toLowerCase()}.m4a`;
+    if (data.name === "WADE" || data.name === "GALE") audio.volume = 0.2; // ear protect
+    else audio.volume = 1;
+    audio.play();
+  }
+  getNode(".volume-control-button").addEventListener("click", (e) => {
+    if (audio.muted) {
+      e.currentTarget.textContent = "음소거";
+      e.currentTarget.style.backgroundColor = "tomato";
+      audio.muted = false;
+    } else {
+      e.currentTarget.textContent = "음소거 해제";
+      e.currentTarget.style.backgroundColor = "dodgerblue";
+      audio.muted = true;
+    }
+    console.log(audio.muted);
+  });
+})();
